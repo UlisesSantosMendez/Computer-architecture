60b19463919e32dd125824795b37ec7390060a2a
Ulises Santos Mendez <96894230+UlisesSantosMendez@users.noreply.github.com>
2023-07-03 19:31:14 -0600

    Add files via upload

---
+++ Prácticas/1er parcial/adder8bitlibuns00/adder8bituns00.vhdl
@@ -0,0 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+library machxo2;
+use machxo2.all;
+library lattice;
+use lattice.all;
+
+entity adder8bituns00 is
+	port(Ai: in std_logic_vector(7 downto 0);
+		 Bi: in std_logic_vector(7 downto 0);
+		 SL: in std_logic;
+		 So: out std_logic_vector(7 downto 0));
+end adder8bituns00;
+
+architecture adder8bituns0 of adder8bituns00 is
+begin
+	with SL select
+	So <= Ai + Bi when '0',
+		  Ai - Bi when '1',
+		  "00000000" when others;
+end adder8bituns0;
